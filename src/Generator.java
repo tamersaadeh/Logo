3f15fbd2a52eaa9bced86de15807861bdb40329b
Tamer Saadeh <tamer@tamersaadeh.com>
2011-03-03 17:17:39 +0200

    moved around the files and create a class to auto generate several logos to decide later on, still empty though

---
+++ src/Generator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author tamer
+ *
+ */
+public class Generator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
